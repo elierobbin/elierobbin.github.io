fc0eafeb6f4621cc103b05109e8ac3079c44fd7c
elierobin <elie.robin@etu.unilim.fr>
2024-11-21 11:47:31 +0100

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://elierobbin.github.io',
 	integrations: [mdx(), sitemap()],
 });
