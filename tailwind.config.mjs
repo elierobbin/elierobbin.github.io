912619622f981878e7e334ecbf295c0939cd3b53
elierobin <elie.robin@etu.unilim.fr>
2025-01-20 10:06:40 +0100

    portfolio new version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			backgroundColor: {
-				'background': '#f2f0df',
-				'black-50': 'rgba(0, 0, 0, 0.75)',
-			},
 		},
 	},
 	plugins: [],
