edb23d429591b5aa0387e5c5242914ce22b802ec
elierobin <elie.robin@etu.unilim.fr>
2025-03-25 18:56:12 +0100

    test

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Elie ROBIN';
-export const LOGO_PATH = "/assets/profil-elie.png";
 export const SITE_DESCRIPTION = 'Bienvenue sur mon portfolio';
